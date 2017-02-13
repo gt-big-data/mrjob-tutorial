644eda753b8a8931d21d71e9e4e3838a669a129e
Sam Marder <sam@marder.co>
2017-02-13 18:51:31 -0500

    Safer Windows fix.

--- wordcount.py
+++ wordcount.py
@@ -16,7 +16,10 @@ def windows_fix():
     # Workaround: Pretend we can't make symlinks and mrjob will work without them.
     # https://github.com/Yelp/mrjob/blob/cc64250308ebf887f4dfe24959f3877a1cd31404/mrjob/sim.py#L123
     if os.name == 'nt':
-        del os.symlink
+        try:
+            del os.symlink
+        except:
+            pass
 
 if __name__ == '__main__':
     windows_fix()
